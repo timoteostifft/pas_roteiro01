45a93927da006b56d20384a4c79355e463d27520
Timóteo Stifft <timoteostifft@gmail.com>
2026-03-10 23:32:59 +0000

    feat: display project info

--- Codespaces/src/App.java
+++ Codespaces/src/App.java
@@ -1,5 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("\nHello world!");
+        String version = System.getProperty("java.version");
+        System.out.println("Running Java Version: "+version+"\n");
+        String name = System.getProperty("user.name");
+        System.out.println("User account name: "+name+"\n");
     }
 }
